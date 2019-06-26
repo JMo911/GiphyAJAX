5a0a08e242482cc2a2bbd7c173a2bdf2bc80c826
Jacob Moss <jacobmoss09@gmail.com>
2019-06-26 16:51:58 -0500

    api responds

--- assets/javascript/myJS.js
+++ assets/javascript/myJS.js
@@ -18,10 +18,32 @@ $( document ).ready(function() {
         var button = $("<button>");
         button.text(topic);
         button.addClass("button btn btn-dark");
+        button.attr("data-name", topic);
         $(".buttons").append(button);
     });
 
-    // $()
+function displaygiphyinfo(){
+    var sport = $(this).attr("data-name");
+    var url="https://api.giphy.com/v1/gifs/search?q=";
+    var apikey="&api_key=RYnKoIeTUvFnDqLxzaqXC9HtLFtkD8mk";
+    var queryurl=url + sport + apikey;
+
+$.ajax({
+    url: queryurl,
+    method: "GET"
+}).then(function(response){
+    console.log(response);
+    // var gifDiv = $("<div>");
+    // gifDiv.append(response);
+    // $(".gifcontainer").append(gifDiv);
+
+});
+}
+
+
+
+$(document).on("click", ".button", displaygiphyinfo);
+
 
 
 
