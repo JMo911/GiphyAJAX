6d60a3ace4cb81b8ae28b781d181ba577b285d54
Jacob Moss <jacobmoss09@gmail.com>
2019-06-28 22:25:26 -0500

    more gifs button is functioning

--- assets/javascript/myJS.js
+++ assets/javascript/myJS.js
@@ -29,27 +29,38 @@ $( document ).ready(function() {
 //call the makebuttons function
 makebuttons();
 
+var params;
+
 
 function displaygiphyinfo(){
     $(".gifcontainer").empty();
     var sport = $(this).attr("data-name");
     var url="https://api.giphy.com/v1/gifs/search?q=";
-    var params="&limit=10&rating=g"
+    params="&limit=200&rating=g";
     var apikey="&api_key=RYnKoIeTUvFnDqLxzaqXC9HtLFtkD8mk";
     var queryurl=url + sport + params + apikey;
+    var arraybegin;
+    var arrayend;
 
-
+    //increase limit to 200 lets say
+    //on initial 'sport button click, append 1-10 (position 0-9)
+    //then, for every 'getmoregifs' button click, append the next 10 
 $.ajax({
     url: queryurl,
     method: "GET"
-}).then(function(response){
-    console.log(response);
+}).then(function renderGifSet(response, arraybegin, arrayend){
+    arraybegin=0;
+    arrayend=10;
+
     var imagearray = response.data;
-    imagearray.forEach(function(element){
+    var thisSlice = imagearray.slice(arraybegin,arrayend);
+    
+    thisSlice.forEach(function(element){
         var picDiv = $("<div>");
         picDiv.addClass("text-center");
         var pic= $("<img>");
-        pic.attr({"src": element.images.fixed_height_still.url,
+        pic.attr({
+                "src": element.images.fixed_height_still.url,
                 "alt": "giphys",
                 "data-state": "still",
                 "data-animateURL": element.images.fixed_height.url,
@@ -58,12 +69,62 @@ $.ajax({
         picDiv.append(pic);
         picDiv.append("<p>Rating: " + element.rating + "</p>");
         $(".gifcontainer").append(picDiv);
+
+
+
+
     })
+
+
+               //MOREGIFBUTTON CLICK EVENT
+               $(document).on("click", ".moregifsbutton", function(event){
+                event.preventDefault();
+                arraybegin +=10;
+                arrayend += 10;
+                console.log(arraybegin, arrayend);
+                thisSlice = imagearray.slice(arraybegin,arrayend);
+                thisSlice.forEach(function(element){
+                     var picDiv = $("<div>");
+                     picDiv.addClass("text-center");
+                     var pic= $("<img>");
+                     pic.attr({
+                         "src": element.images.fixed_height_still.url,
+                         "alt": "giphys",
+                         "data-state": "still",
+                         "data-animateURL": element.images.fixed_height.url,
+                         "data-stillURL": element.images.fixed_height_still.url,
+                         "class": "img-fluid"
+                     });
+                     picDiv.append(pic);
+                     picDiv.append("<p>Rating: " + element.rating + "</p>");
+                     $(".gifcontainer").append(picDiv);
+                }); 
+                
+                
+                
+ 
+     
+         });
+
+    //MORE GIFS BUTTON
+    //as soon as they click this button, just append the next 10 that are in my response
+    var moreGifsButtonDiv = $("<div>");
+    moreGifsButtonDiv.addClass("col-12");
+    var moreGifsButton = $("<button>");
+    moreGifsButton.addClass("btn btn-dark moregifsbutton");
+    moreGifsButton.text("Click here for more GIFs");
+    moreGifsButtonDiv.append(moreGifsButton);
+    $(".gifcontainer").append(moreGifsButtonDiv);
+
 
 
 
 });
+
+
 }
+
+
 
 
 
